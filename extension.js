47e52148cae7f3bc037b7921cd7a74c470d7315d
CloretoSiegh <36214504+CloretoSiegh@users.noreply.github.com>
2018-02-07 00:01:02 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "basicBot";
+    var fork = "botFudido";
 
     // Define our function responsible for extending the bot.
     function extend() {
@@ -45,11 +45,11 @@
 
     //Change the bots default settings and make sure they are loaded on launch
 
-    localStorage.setItem("basicBotsettings", JSON.stringify({
+    localStorage.setItem("botFudidosettings", JSON.stringify({
         botName: 'botFudido',
         language: 'portuguese',
         chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
-        scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
+        scriptLink: 'https://rawgit.com/CloretoSiegh/botloco/master/extension.js',
         roomLock: false, // Requires an extension to re-load the script
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
